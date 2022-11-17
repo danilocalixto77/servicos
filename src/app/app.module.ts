b7b328955db8257459a1928e4a411bb3698d5fc4
Danilo Calixto <danilo.calixto@sblogpvh.local>
2022-11-17 17:31:30 -0400

    CursoAngular#42#InjetandoService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { AppComponent } from './app.component';
 import { CursosService } from './cursos/curso.service';
 import { CriarCursoModule } from './criar-curso/criar-curso.module';
 import { CursosModule } from './cursos/cursos.module';
+import { LogService } from './shared/log.service';
 
 
 @NgModule({
@@ -23,7 +24,8 @@ import { CursosModule } from './cursos/cursos.module';
     CursosModule
 
   ],
-  providers: [CursosService],
+  providers:[LogService],
+  //providers: [CursosService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
--- src/app/cursos/curso.service.ts
+++ src/app/cursos/curso.service.ts
@@ -1,4 +1,5 @@
 import { EventEmitter, Injectable } from '@angular/core'
+import { LogService } from '../shared/log.service';
 @Injectable()
 export class CursosService{
 
@@ -7,15 +8,17 @@ export class CursosService{
 
   private cursos: string[] = ['Angular2', 'Java', 'Phonegap'];
 
-  constructor(){
+  constructor(private logService: LogService){
     console.log('CursosService');
   }
 
   getCursos(){
+    this.logService.consoleLog('Obtendo lista de cursos.')
     return this.cursos;
   }
 
   addCursos(curso: string){
+    this.logService.consoleLog(`Criando um novo curso ${curso}`)
     this.cursos.push(curso);
     this.emitirCursosCriado.emit(curso);
     CursosService.criouNovoCurso.emit(curso);
